f3a79e2e48958e73de8fc99a99ae9d4df58a48a2
ivan <ivan.verbitsk@ya.ru>
2020-04-10 12:54:04 +0300

    Структура базы данных, дополнение док. и макетов

---
+++ bd/aroma_candles.sql
@@ -0,0 +1,365 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Апр 10 2020 г., 12:33
+-- Версия сервера: 10.3.13-MariaDB-log
+-- Версия PHP: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `aroma_candles`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `aromas`
+--
+
+CREATE TABLE `aromas` (
+  `id_aroma` smallint(5) NOT NULL,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `quantity` mediumint(7) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `candles`
+--
+
+CREATE TABLE `candles` (
+  `id_candle` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_color` smallint(5) NOT NULL,
+  `id_shape` smallint(5) NOT NULL,
+  `id_aroma` smallint(5) NOT NULL,
+  `id_size` smallint(5) NOT NULL,
+  `id_category` tinyint(3) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `quantity` mediumint(7) NOT NULL,
+  `total_sold` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `categories`
+--
+
+CREATE TABLE `categories` (
+  `id_category` tinyint(3) NOT NULL,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `colors`
+--
+
+CREATE TABLE `colors` (
+  `id_color` smallint(5) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `quantity` mediumint(7) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `details_order`
+--
+
+CREATE TABLE `details_order` (
+  `id_position` int(11) NOT NULL,
+  `id_order` int(11) NOT NULL,
+  `id_candle` int(11) NOT NULL,
+  `quantity` int(7) NOT NULL,
+  `position_sum` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `orders`
+--
+
+CREATE TABLE `orders` (
+  `id_order` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `total_sum` decimal(10,2) NOT NULL,
+  `date_of_start` date NOT NULL,
+  `date_of_end` date DEFAULT NULL,
+  `id_status` tinyint(3) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `roles`
+--
+
+CREATE TABLE `roles` (
+  `id_role` tinyint(2) NOT NULL,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `shapes`
+--
+
+CREATE TABLE `shapes` (
+  `id_shape` smallint(5) NOT NULL,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `quantity` mediumint(7) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `sizes`
+--
+
+CREATE TABLE `sizes` (
+  `id_size` smallint(5) NOT NULL,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `quantity` mediumint(7) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `statuses`
+--
+
+CREATE TABLE `statuses` (
+  `id_status` tinyint(3) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int(11) NOT NULL,
+  `telephone` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `first_name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `second_name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `id_role` tinyint(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `aromas`
+--
+ALTER TABLE `aromas`
+  ADD PRIMARY KEY (`id_aroma`);
+
+--
+-- Индексы таблицы `candles`
+--
+ALTER TABLE `candles`
+  ADD PRIMARY KEY (`id_candle`),
+  ADD KEY `id_color` (`id_color`),
+  ADD KEY `id_shape` (`id_shape`),
+  ADD KEY `id_aroma` (`id_aroma`),
+  ADD KEY `id_size` (`id_size`),
+  ADD KEY `id_category` (`id_category`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Индексы таблицы `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id_category`);
+
+--
+-- Индексы таблицы `colors`
+--
+ALTER TABLE `colors`
+  ADD PRIMARY KEY (`id_color`);
+
+--
+-- Индексы таблицы `details_order`
+--
+ALTER TABLE `details_order`
+  ADD PRIMARY KEY (`id_position`),
+  ADD KEY `id_order` (`id_order`),
+  ADD KEY `id_candle` (`id_candle`);
+
+--
+-- Индексы таблицы `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id_order`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_status` (`id_status`);
+
+--
+-- Индексы таблицы `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id_role`);
+
+--
+-- Индексы таблицы `shapes`
+--
+ALTER TABLE `shapes`
+  ADD PRIMARY KEY (`id_shape`);
+
+--
+-- Индексы таблицы `sizes`
+--
+ALTER TABLE `sizes`
+  ADD PRIMARY KEY (`id_size`);
+
+--
+-- Индексы таблицы `statuses`
+--
+ALTER TABLE `statuses`
+  ADD PRIMARY KEY (`id_status`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`),
+  ADD KEY `id_role` (`id_role`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `aromas`
+--
+ALTER TABLE `aromas`
+  MODIFY `id_aroma` smallint(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `candles`
+--
+ALTER TABLE `candles`
+  MODIFY `id_candle` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id_category` tinyint(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `colors`
+--
+ALTER TABLE `colors`
+  MODIFY `id_color` smallint(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `details_order`
+--
+ALTER TABLE `details_order`
+  MODIFY `id_position` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id_order` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id_role` tinyint(2) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `shapes`
+--
+ALTER TABLE `shapes`
+  MODIFY `id_shape` smallint(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `sizes`
+--
+ALTER TABLE `sizes`
+  MODIFY `id_size` smallint(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `statuses`
+--
+ALTER TABLE `statuses`
+  MODIFY `id_status` tinyint(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `candles`
+--
+ALTER TABLE `candles`
+  ADD CONSTRAINT `candles_ibfk_1` FOREIGN KEY (`id_color`) REFERENCES `colors` (`id_color`),
+  ADD CONSTRAINT `candles_ibfk_2` FOREIGN KEY (`id_shape`) REFERENCES `shapes` (`id_shape`),
+  ADD CONSTRAINT `candles_ibfk_3` FOREIGN KEY (`id_aroma`) REFERENCES `aromas` (`id_aroma`),
+  ADD CONSTRAINT `candles_ibfk_4` FOREIGN KEY (`id_size`) REFERENCES `sizes` (`id_size`),
+  ADD CONSTRAINT `candles_ibfk_5` FOREIGN KEY (`id_category`) REFERENCES `categories` (`id_category`),
+  ADD CONSTRAINT `candles_ibfk_6` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`);
+
+--
+-- Ограничения внешнего ключа таблицы `details_order`
+--
+ALTER TABLE `details_order`
+  ADD CONSTRAINT `details_order_ibfk_1` FOREIGN KEY (`id_order`) REFERENCES `orders` (`id_order`),
+  ADD CONSTRAINT `details_order_ibfk_2` FOREIGN KEY (`id_candle`) REFERENCES `candles` (`id_candle`);
+
+--
+-- Ограничения внешнего ключа таблицы `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`),
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`id_status`) REFERENCES `statuses` (`id_status`);
+
+--
+-- Ограничения внешнего ключа таблицы `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`id_role`) REFERENCES `roles` (`id_role`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
